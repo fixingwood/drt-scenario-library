8d9cdbed90752ea1a353fd889386cec66a02c6e3
zhanglichieh <richardchang3@gmail.com>
2026-05-25 23:42:49 +0800

    Update plan file to latest version, and update hte config file to a reasonable flow capacity and storage capacity

--- src/main/java/org/matsim/run/RunSEQBasic.java
+++ src/main/java/org/matsim/run/RunSEQBasic.java
@@ -11,21 +11,21 @@ import org.matsim.simwrapper.SimWrapperModule;
 public class RunSEQBasic {
 
     public static void main(String[] args) {
-        // 1. 載入 Config，必須包含 MultiModeDrtConfigGroup 與 DvrpConfigGroup
-        // args[0] 應為 config.xml 的路徑
+        // 1. Loading Config, including  MultiModeDrtConfigGroup and DvrpConfigGroup modules
+        // args[0] should be the route of config.xml
         String configFile = args.length > 0 ? args[0] : "scenarios/SEQ/SEQ_Basic.xml";
 
         Config config = ConfigUtils.loadConfig(configFile);
 
-        // 2. 建立 Scenario
+        // 2. Build Scenario
         Scenario scenario = ScenarioUtils.loadScenario(config);
 
-        // 3. 建立 Controler
+        // 3. Build Controler
         Controler controler = new Controler(scenario);
         controler.addOverridingModule(new SimWrapperModule());
 
 
-        // 5. 啟動模擬
+        // 5. start simulation
         controler.run();
     }
 }
